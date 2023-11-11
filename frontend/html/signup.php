6fd212c94fa826676ee79c08fbd8916f2c3ad9b2
Adegbehingbe, Oluwakorede Joseph <oluwakoredeadegbehingbe@gmail.com>
2023-11-11 11:26:47 +0100

    home page complete

---
+++ frontend/html/signup.php
@@ -0,0 +1,38 @@
+<!-- HEADER -->
+<?php
+    $pageTitle = 'Sign Up';
+    ob_start();
+?>
+
+<!-- Styles -->
+<link rel="stylesheet" href="frontend/styles/index/index.css">
+
+<?php
+    $styles = ob_get_clean();
+?>
+
+<!-- ---------------------------------------------------------------------------------------------------- -->
+<!-- ---------------------------------------------------------------------------------------------------- -->
+
+<!-- CONTENT -->
+<?php
+    ob_start();
+?>
+
+<!-- Main Content -->
+<section id="hero">
+    <div class="hero-content">
+        <h1>Welcome to LitShelf Online Library!</h1>
+        <h3>📚 Explore, Discover, and Immerse Yourself in the World of Books 🌍</h3>
+        <a href="#" class="button">Get Started</a>
+    </div>
+
+    <div class="hero-img">
+        <img src="frontend/assets/images/home/home-hero.jpg" alt="hero-image">
+    </div>
+</section>
+
+<?php
+    $content = ob_get_clean();
+    include 'base.php';
+?>
--- index.php
+++ index.php
@@ -1,11 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LitShelf- Home</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+    // Determine which page to load based on the URL
+    $path = $_SERVER['REQUEST_URI'];
+
+    echo $path;
+
+    if ($path === '/LitShelf/signup') {
+        include 'frontend/html/signup.php';
+    } else {
+        include 'frontend/html/home.php';
+    }
+?>
